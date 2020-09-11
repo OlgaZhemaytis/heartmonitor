901177db02014b76a6e6aaefcffbe11c3b8ea335
OlgaZH <your_email@whatever.com>
2020-09-11 21:38:58 +0300

    all without mail_demp

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,124 @@
 $(document).ready(function(){
-	$('.carusel__inner').slick({
-		speed: 1500,
+    $('.carusel__inner').slick({
+        speed: 1200,
+		adaptiveHeight: false,
 		autoplay: true,
-		autoplaySpeed: 3000,
-		prevArrow: '<button type="button" class="slick-prev"><img src="../src/icon/left.svg"></button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="../src/icon/right.svg"></button>',
-		responsive: [
-		 	{
-		 		breakpoint: 992,
-		 		settings: {
-		 		  dots: true,
-		 		  arrows: false
-				 }
+        prevArrow: '<button type="button" class="slick-prev"><img src="../src/icon/left.svg""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../src/icon/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+					arrows: false,
+					pauseOnDotsHover: true,
+                }
+            }
+        ]
+    });
+//переключение выборки табоми   
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+//перелистывание окна таба
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+	toggleSlide('.catalog-item__back');
+// открытие и закрытие модальных окон	
+	$('[data-modal=consultation]').on('click', function(){
+		$('.overlay, #consult' ).fadeIn('slow');
+	});
+	$('.modul__close').on('click',function(){
+		$('.overlay, #order, #ok').fadeOut('slow');
+	});
+// изменение названия приобретаемого продутка в форме в соответствии с карточкой
+	$('.button_mini').each(function(i){
+		$(this).on('click', function(){
+			$('#order .modul__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order' ).fadeIn('slow');
+		})
+	});
+//валидация ворм отправки
+	function validateForms(form){
+		$(form).validate({
+			rules: {
+				name: {
+					required: true,
+					minlength: 2
+				},
+				phone: "required",
+				email: {
+					required: true,
+					email: true
+				}
+			},
+			messages: {
+				name: {
+					required: "Пожалуйста, введите свое имя",
+					minlength: jQuery.validator.format("Введите {0} символа!")
+				},
+				phone: "Пожалуйста, введите свой номер телефона",
+				email: {
+				required: "Пожалуйста, введите свою почту",
+				email: "Неправильно введен адрес почты"
+				}
 			}
-		 ]
-	  });
-  });
+			
+		});
+
+	}
+	validateForms('#consult-form');
+	validateForms('#consult form');
+	validateForms('#order form');
+//маска телефонных номеров
+	$('input[name=phone]').mask("+7 (999) 999-99-99");
+//скрипт для отправки сообщений с сайта( jnv)
+	$('form').submit(function(e) {
+		e.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			$('#consultation, #order').fadeOut();
+			$('.overlay, #thanks').fadeIn('slow');
+
+			$('form').trigger('reset');
+		});
+		return false;
+	});
+	// плавная прокрутка стрелочкой
+	$(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#up']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+	});
+
+	$("a[href^='#cat']").click(function(){
+        const _href1 = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href1).offset().top+"px"});
+        return false;
+    });
+	new WOW().init();
+
+});
